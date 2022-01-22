dad3568924c56ca968539074ad88ca70174f9d42
Ryferion <42130642+Ryferion@users.noreply.github.com>
2022-01-21 23:09:45 -0800

    Update ipc_client.py

--- client/ipc_client.py
+++ client/ipc_client.py
@@ -10,15 +10,22 @@ PORT = 9002
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Connected!')
-    f = open ("mydata.txt", "wb")
-    while 1:
-        data = ''
-        data = s.recv(1024)
-        if data == b'': 
-            break
-        f.write(data)
-    f.close()
+    s.sendall(b'Hello, world. IPC success!')
+    data = s.recv(1024)
+
+print('Received', repr(data))
+
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.connect((HOST, PORT))
+#     s.sendall(b'Connected!')
+#     f = open ("mydata.txt", "wb")
+#     while 1:
+#         data = ''
+#         data = s.recv(1024)
+#         if data == b'': 
+#             break
+#         f.write(data)
+#     f.close()
     # data = f.read(1024)
     #while (data):
     #    s.sendall(b'Hello! Receiving file!')
