0d807c2ab177e9489695836070051137a242b4b8
Ryferion <42130642+Ryferion@users.noreply.github.com>
2022-01-21 18:05:00 -0800

    Update ipc_server.py

--- ipc_server.py
+++ ipc_server.py
@@ -1,17 +1,27 @@
 import socket
+import sys
 
 HOST = '0.0.0.0'
 PORT = 9002
+
+def send_file(self, name):
+  with open('./sampletest/' + name, 'rb') as handle:
+    return handle.read()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   s.bind((HOST, PORT))
   s.listen()
   conn, addr = s.accept()
   
+  print addr
+
   with conn:
     print('Connected by', addr)
+    #f = open("mydata.txt", 'rb')
+    
     while True:
       data = conn.recv(1024)
       if not data:
          break
+      #data = f.read(1024)
       conn.sendall(data)
