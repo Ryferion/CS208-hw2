ec5c465e98fa3acdf489283c5bdc655e11c64478
Ryferion <42130642+Ryferion@users.noreply.github.com>
2022-01-21 18:05:23 -0800

    Update ipc_server.py

--- ipc_server.py
+++ ipc_server.py
@@ -12,8 +12,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   s.bind((HOST, PORT))
   s.listen()
   conn, addr = s.accept()
-  
-  print addr
 
   with conn:
     print('Connected by', addr)
