ab15986d68440b862d1f410f1e1d6cd8aa487fd0
Ryferion <42130642+Ryferion@users.noreply.github.com>
2022-01-21 18:11:41 -0800

    Update ipc_server.py

--- ipc_server.py
+++ ipc_server.py
@@ -12,14 +12,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   s.bind((HOST, PORT))
   s.listen()
   conn, addr = s.accept()
-
+  
   with conn:
     print('Connected by', addr)
-    #f = open("mydata.txt", 'rb')
+    f = open("mydata.txt", 'rb')
     
     while True:
-      data = conn.recv(1024)
+      #data = conn.recv(1024)
+      data = f.read(1024)
       if not data:
          break
-      #data = f.read(1024)
       conn.sendall(data)
